29b604f2e665ab36fd5b2d9a593bf9240baa0ba4
ahao <alexah9290@gmail.com>
2025-01-03 11:26:17 -0600

    Added igw and route table

--- terraform/a4l_modules/network/main.tf
+++ terraform/a4l_modules/network/main.tf
@@ -13,14 +13,56 @@ resource "aws_vpc" "a4l_vpc1" {
 resource "aws_subnet" "a4l_subnets" {
   for_each = var.subnets_map
 
-  vpc_id            = aws_vpc.a4l_vpc1.id
-  availability_zone = join("", [var.region, each.value.availability_zone])
-  cidr_block        = each.value.cidr_block_ipv4
+  vpc_id                          = aws_vpc.a4l_vpc1.id
+  availability_zone               = join("", [var.region, each.value.availability_zone])
+  cidr_block                      = each.value.cidr_block_ipv4
   ipv6_cidr_block                 = join("", [replace(aws_vpc.a4l_vpc1.ipv6_cidr_block, "/00::/56/", ""), each.value.cidr_block_ipv6])
   assign_ipv6_address_on_creation = true
+  map_public_ip_on_launch         = each.value.auto_assign_ipv4
 
   tags = {
     Name = each.key
     Type = "a4l_subnet"
   }
+}
+
+resource "aws_internet_gateway" "a4l-vpc1-igw" {
+  vpc_id = aws_vpc.a4l_vpc1.id
+
+  tags = {
+    Name = "a4l-vpc1-igw_tf"
+  }
+}
+
+resource "aws_route_table" "a4l-vpc1-rt-web" {
+  vpc_id = aws_vpc.a4l_vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.a4l-vpc1-igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.a4l-vpc1-igw.id
+  }
+
+  tags = {
+    Name = "a4l-vpc1-rt-web-tf"
+  }
+}
+
+resource "aws_route_table_association" "rta-a" {
+  subnet_id      = aws_subnet.a4l_subnets["sn-web-A"].id
+  route_table_id = aws_route_table.a4l-vpc1-rt-web.id
+}
+
+resource "aws_route_table_association" "rta-b" {
+  subnet_id      = aws_subnet.a4l_subnets["sn-web-B"].id
+  route_table_id = aws_route_table.a4l-vpc1-rt-web.id
+}
+
+resource "aws_route_table_association" "rta-c" {
+  subnet_id      = aws_subnet.a4l_subnets["sn-web-C"].id
+  route_table_id = aws_route_table.a4l-vpc1-rt-web.id
 }
--- terraform/a4l_modules/network/variables.tf
+++ terraform/a4l_modules/network/variables.tf
@@ -8,67 +8,80 @@ variable "subnets_map" {
     availability_zone = string
     cidr_block_ipv4   = string
     cidr_block_ipv6   = string
+    auto_assign_ipv4  = bool
   }))
   default = {
     sn-reserved-A = {
       availability_zone = "a"
       cidr_block_ipv4   = "10.16.0.0/20"
       cidr_block_ipv6   = "00::/64"
+      auto_assign_ipv4  = false
     }
     sn-db-A = {
       availability_zone = "a"
       cidr_block_ipv4   = "10.16.16.0/20"
       cidr_block_ipv6   = "01::/64"
+      auto_assign_ipv4  = false
     }
     sn-app-A = {
       availability_zone = "a"
       cidr_block_ipv4   = "10.16.32.0/20"
       cidr_block_ipv6   = "02::/64"
+      auto_assign_ipv4  = false
     }
     sn-web-A = {
       availability_zone = "a"
       cidr_block_ipv4   = "10.16.48.0/20"
       cidr_block_ipv6   = "03::/64"
+      auto_assign_ipv4  = true
     }
     sn-reserved-B = {
       availability_zone = "b"
       cidr_block_ipv4   = "10.16.64.0/20"
       cidr_block_ipv6   = "04::/64"
+      auto_assign_ipv4  = false
     }
     sn-db-B = {
       availability_zone = "b"
       cidr_block_ipv4   = "10.16.80.0/20"
       cidr_block_ipv6   = "05::/64"
+      auto_assign_ipv4  = false
     }
     sn-app-B = {
       availability_zone = "b"
       cidr_block_ipv4   = "10.16.96.0/20"
       cidr_block_ipv6   = "06::/64"
+      auto_assign_ipv4  = false
     }
     sn-web-B = {
       availability_zone = "b"
       cidr_block_ipv4   = "10.16.112.0/20"
       cidr_block_ipv6   = "07::/64"
+      auto_assign_ipv4  = true
     }
     sn-reserved-C = {
       availability_zone = "c"
       cidr_block_ipv4   = "10.16.128.0/20"
       cidr_block_ipv6   = "08::/64"
+      auto_assign_ipv4  = false
     }
     sn-db-C = {
       availability_zone = "c"
       cidr_block_ipv4   = "10.16.144.0/20"
       cidr_block_ipv6   = "09::/64"
+      auto_assign_ipv4  = false
     }
     sn-app-C = {
       availability_zone = "c"
       cidr_block_ipv4   = "10.16.160.0/20"
-      cidr_block_ipv6   = "0A::/64"
+      cidr_block_ipv6   = "0a::/64"
+      auto_assign_ipv4  = false
     }
     sn-web-C = {
       availability_zone = "c"
       cidr_block_ipv4   = "10.16.176.0/20"
-      cidr_block_ipv6   = "0B::/64"
+      cidr_block_ipv6   = "0b::/64"
+      auto_assign_ipv4  = true
     }
   }
 }
