1948211de5310b41cfb34c9479bfd68864b7ee93
ahao <alexah9290@gmail.com>
2025-01-02 18:50:28 -0600

    Creating a4l vpc subnets

--- terraform/a4l_modules/network/main.tf
+++ terraform/a4l_modules/network/main.tf
@@ -14,9 +14,10 @@ resource "aws_subnet" "a4l_subnets" {
   for_each = var.subnets_map
 
   vpc_id            = aws_vpc.a4l_vpc1.id
-  availability_zone = "${region} + each.value.availability_zone"
+  availability_zone = join("", [var.region, each.value.availability_zone])
   cidr_block        = each.value.cidr_block_ipv4
-  ipv6_cidr_block   = aws_vpc.a4l_vpc1.ipv6_cidr_block + each.value.cidr_block_ipv6
+  # https://developer.hashicorp.com/terraform/language/functions/replace
+  ipv6_cidr_block = join("", [replace(aws_vpc.a4l_vpc1.ipv6_cidr_block, "/00::/56/", ""), each.value.cidr_block_ipv6])
 
   tags = {
     Name = each.key
