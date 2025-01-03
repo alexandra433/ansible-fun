31ea40a2cd6dbea7af0957b0204f9bb079bfda6f
ahao <alexah9290@gmail.com>
2025-01-02 19:04:19 -0600

    Some cleanup

--- terraform/a4l_modules/network/main.tf
+++ terraform/a4l_modules/network/main.tf
@@ -16,7 +16,6 @@ resource "aws_subnet" "a4l_subnets" {
   vpc_id            = aws_vpc.a4l_vpc1.id
   availability_zone = join("", [var.region, each.value.availability_zone])
   cidr_block        = each.value.cidr_block_ipv4
-  # https://developer.hashicorp.com/terraform/language/functions/replace
   ipv6_cidr_block                 = join("", [replace(aws_vpc.a4l_vpc1.ipv6_cidr_block, "/00::/56/", ""), each.value.cidr_block_ipv6])
   assign_ipv6_address_on_creation = true
 
